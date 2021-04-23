f27e52b372f4a772e7ec5f0ab4984e99addd9385
何东升 <he77505357@163.com>
2021-04-23 16:02:44 +0800

    feat(webpack.config.js): webpack-dev-server

--- webpack.config.js
+++ webpack.config.js
@@ -2,16 +2,21 @@ const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
+    mode: 'development',// 开发环境
     entry: {
         index: './src/index.js',
         print: './src/print.js',
     },
+    devtool: 'inline-source-map',
     plugins: [
         new HtmlWebpackPlugin({
-            title: '管理输出-HtmlWebpackPlugin',
+            title: 'Output Management',
             template: './src/index.html',
         }),
     ],
+    devServer: {
+        contentBase: './dist',
+    },
     output: {
         filename: '[name].bundle.js',
         path: path.resolve(__dirname, 'dist'),
